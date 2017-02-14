a124121d0388ff8eeedba240f0c6ea6b0f8608b7
Julie H Engel <engelju@eniac.seas.upenn.edu>
2017-02-14 01:39:03 -0500

    bleichenbacher initial

--- bleichenbacher.py
+++ bleichenbacher.py
@@ -1,28 +1,24 @@
-from Crypto.Hash import SHA
-from Crypto.PublicKey import RSA
 from roots import *
+from Crypto.Hash import SHA
 import sys
-
-message1 = "cis331+engelju+5.00"
+import math
 
 message = sys.argv[1]
 
-pubkey = """-----BEGIN PUBLIC KEY-----
-MIIBIDANBgkqhkiG9w0BAQEFAAOCAQ0AMIIBCAKCAQEA/eR8aHozuAhH22kyj0MD
-Sq7Ljupvho3qJFOEpgAD3AFj58pPzXJRd2vEIAUy49dhM+5Dv2x0GTBPHNMOyCJd
-F/lzG0AbYqMmtpcdswrj13Vq+SOGT3o/ulnHWt8GTS6sFFtmC9GgzT4mXUwzTkpl
-f6vbM715qBlPQifiqahFUgoq9bmhc1+i0WNNt6rINABB74lOlCKNBvhnGH3apgRr
-sdu+eI6+DsGPipRUL3ghP8wg6+Dbl5iH+tsgaaYpLQNaW8frD692PCYtH/6ghSRu
-4ipKPOvBlRfXyWeq+rLcrAMsNcczwLPdBG1SiVrM+woh935baPDRpv0dAzlIhsfw
-6wIBAw==
------END PUBLIC KEY-----"""
+# Your code to forge a signature goes here.
+hex_string = "0001"
+for i in range(0, 90):
+	hex_string += "FF"
+hex_string += "003021300906052B0E03021A05000414"
+hex_string += SHA.new(message).hexdigest()
+#print hex_string
+for i in range(0, 128):
+	hex_string += "01"
+#print hex_string
+dec = bytes_to_integer(hex_string.decode("hex"))
+# print math.pow(2, 1024)
+# dec *= math.pow(2, 1024)
+# print dec
 
-pad = "" #fix this
-asn1 = "3021300906052B0E03021A05000414"
-sha = SHA.new(message).hexdigest()
-
-
-
-# main testing
-pk = RSA.importKey(pubkey)
-
+root, is_exact = integer_nthroot(dec, 3)
+print integer_to_base64(root)
