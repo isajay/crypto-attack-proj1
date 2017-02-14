7685897df904200e5b5604bfbb714ea52b4b4ee1
Julie H Engel <engelju@eniac.seas.upenn.edu>
2017-02-14 16:15:29 -0500

    working

--- bleichenbacher.py
+++ bleichenbacher.py
@@ -7,13 +7,14 @@ message = sys.argv[1]
 
 # Your code to forge a signature goes here.
 hex_string = "0001"
-for i in range(0, 90):
+for i in range(0, 48):
 	hex_string += "FF"
 hex_string += "003021300906052B0E03021A05000414"
 hex_string += SHA.new(message).hexdigest()
 #print hex_string
-for i in range(0, 128):
-	hex_string += "01"
+
+for i in range(0, 512 - len(hex_string)):
+	hex_string += "F"
 #print hex_string
 dec = bytes_to_integer(hex_string.decode("hex"))
 # print math.pow(2, 1024)
@@ -21,4 +22,8 @@ dec = bytes_to_integer(hex_string.decode("hex"))
 # print dec
 
 root, is_exact = integer_nthroot(dec, 3)
+#diff = dec - (root * root * root)
+#perfect_cube = dec - diff
+#print "Perfect cube: " + str(perfect_cube)
+#print "Root: " + str(root)
 print integer_to_base64(root)
